24612bbfabc2181d5384d0bfd431f72a6649fa87
Avenger <Dima_c.2011@mail.ru>
2021-06-12 18:05:17 +0300

    Задача на Инкапсуляцию

---
+++ Java_Core/src/main/java/Level_1/lvl_1_task_10.java
@@ -0,0 +1,37 @@
+package Level_1;
+
+/*
+Не забываем инкапсулировать
+*/
+
+public class lvl_1_task_10 {
+  public static void main(String[] args) {
+  }
+
+  public class Cat {
+    private String name;
+    private int age;
+    private int weight;
+    private int speed;
+
+    public Cat(String name, int age, int weight) {
+
+    }
+
+    public String getName() {
+      return null;
+    }
+
+    public int getAge() {
+      return 0;
+    }
+
+    public void setWeight(int weight) {
+
+    }
+
+    public void setSpeed(int speed) {
+
+    }
+  }
+}
