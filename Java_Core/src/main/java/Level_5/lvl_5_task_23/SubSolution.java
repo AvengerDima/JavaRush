766f332b73367418947cb6515cfaf24c9ee965d0
Avenger <Dima_c.2011@mail.ru>
2021-06-13 10:08:12 +0300

    Перегрузка Конструкторов

---
+++ Java_Core/src/main/java/Level_5/lvl_5_task_23/SubSolution.java
@@ -0,0 +1,14 @@
+package Level_5.lvl_5_task_23;
+
+public class SubSolution extends lvl_5_task_23 {
+  SubSolution(String a) {
+    super(a);
+  }
+
+  public SubSolution() {
+  }
+
+  protected SubSolution(Integer a) {
+    super(a);
+  }
+}
---
+++ Java_Core/src/main/java/Level_5/lvl_5_task_23/lvl_5_task_23.java
@@ -0,0 +1,27 @@
+package Level_5.lvl_5_task_23;
+
+/*
+Перегрузка конструкторов
+*/
+
+public class lvl_5_task_23 {
+  public static void main(String[] args) {
+
+  }
+
+  lvl_5_task_23(String a) {
+
+  }
+
+  public lvl_5_task_23() {
+
+  }
+
+  private lvl_5_task_23(int a) {
+
+  }
+
+  protected lvl_5_task_23(Integer a) {
+
+  }
+}
