c761c61dbcd03eba7bcebfe9a00a0772d3e6fed2
Avenger <Dima_c.2011@mail.ru>
2021-06-13 13:49:40 +0300

    Знакомство с нитями: Thread,Runnable, start, join, interrupt, sleep

---
+++ Java_Core/src/main/java/Level_6/lvl_6_task_31/common/ImageTypes.java
@@ -0,0 +1,7 @@
+package Level_6.lvl_6_task_31.common;
+
+public enum ImageTypes {
+  BMP,
+  JPG,
+  PNG
+}
---
+++ Java_Core/src/main/java/Level_6/lvl_6_task_31/common/JpgReader.java
@@ -0,0 +1,4 @@
+package Level_6.lvl_6_task_31.common;
+
+public class JpgReader implements ImageReader {
+}
