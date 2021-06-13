e2b867bcefe49d7c6a6422cb2368d461bfe9c621
Avenger <Dima_c.2011@mail.ru>
2021-06-13 12:32:28 +0300

    Лекарство

---
+++ Java_Core/src/main/java/Level_7/lvl_7_task_13/Drug.java
@@ -0,0 +1,22 @@
+package Level_7.lvl_7_task_13;
+
+public class Drug {
+  private String name;
+  private String description;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
