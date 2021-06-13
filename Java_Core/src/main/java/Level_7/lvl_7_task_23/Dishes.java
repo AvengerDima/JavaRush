85ebd1e46110c342d5c736bfac1714e8da897ce3
Avenger <Dima_c.2011@mail.ru>
2021-06-13 12:43:11 +0300

    Блюда

---
+++ Java_Core/src/main/java/Level_7/lvl_7_task_23/Dishes.java
@@ -0,0 +1,13 @@
+package Level_7.lvl_7_task_23;
+
+public class Dishes {
+  private byte tableNumber;
+
+  public Dishes(byte tableNumber) {
+    this.tableNumber = tableNumber;
+  }
+
+  public byte getTableNumber() {
+    return tableNumber;
+  }
+}
