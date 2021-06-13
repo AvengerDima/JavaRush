8f7d49284d2d0abb2fea6b3b309a9a616361906c
Avenger <Dima_c.2011@mail.ru>
2021-06-13 09:59:54 +0300

    Задача на ООП

---
+++ Java_Core/src/main/java/Level_5/lvl_5_task_13.java
@@ -0,0 +1,22 @@
+package Level_5;
+
+/*
+Максимально простой код-3
+*/
+
+public class lvl_5_task_13 {
+  public static void main(String[] args) {
+  }
+
+  public interface Runnable {
+
+  }
+
+  public class Machine implements Runnable {
+
+  }
+
+  public class Car extends Machine {
+
+  }
+}
