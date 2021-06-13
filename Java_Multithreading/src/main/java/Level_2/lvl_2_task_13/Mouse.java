7ac73c72e9d0284060c998163fbf5f81bcaeb59d
Avenger <Dima_c.2011@mail.ru>
2021-06-13 14:51:08 +0300

    Мышь

---
+++ Java_Multithreading/src/main/java/Level_2/lvl_2_task_13/Mouse.java
@@ -0,0 +1,19 @@
+package Level_2.lvl_2_task_13;
+
+public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
