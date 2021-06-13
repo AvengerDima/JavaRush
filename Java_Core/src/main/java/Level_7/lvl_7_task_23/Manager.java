0865758c2d891d6ff58b72a71c4f9cd1d09d9b4c
Avenger <Dima_c.2011@mail.ru>
2021-06-13 12:43:54 +0300

    Менеджер

---
+++ Java_Core/src/main/java/Level_7/lvl_7_task_23/Manager.java
@@ -0,0 +1,40 @@
+package Level_7.lvl_7_task_23;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Manager {
+  private static Manager ourInstance = new Manager();
+
+  private final List<Table> restaurantTables = new ArrayList<Table>(10);
+  private int currentIndex = 0;
+
+  private final Queue<Order> orderQueue = new ConcurrentLinkedQueue<Order>();        // очередь с заказами
+  private final Queue<Dishes> dishesQueue = new ConcurrentLinkedQueue<Dishes>();     // очередь с готовыми блюдами
+
+  public synchronized static Manager getInstance() {
+    return ourInstance;
+  }
+
+  private Manager() {               // создаем 10 столов
+    for (int i = 0; i < 10; i++) {
+      restaurantTables.add(new Table());
+    }
+  }
+
+  public synchronized Table getNextTable() {           // официант ходит по кругу от 1 стола к 10
+    Table table = restaurantTables.get(currentIndex);
+    currentIndex = (currentIndex + 1) % 10;
+    return table;
+  }
+
+  public Queue<Order> getOrderQueue() {
+    return orderQueue;
+  }
+
+  public Queue<Dishes> getDishesQueue() {
+    return dishesQueue;
+  }
+}
