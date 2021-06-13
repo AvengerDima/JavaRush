0c8fb2d73cfac94e36863ecfb1dc06d4b73c018a
Avenger <Dima_c.2011@mail.ru>
2021-06-13 12:14:48 +0300

    Factory Method Pattern

---
+++ Java_Core/src/main/java/Level_6/lvl_6_task_31/ImageReaderFactory.java
@@ -0,0 +1,16 @@
+package Level_6.lvl_6_task_31;
+
+import Java_Core.Level_6.lvl_6_task_31.common.*;
+
+public class ImageReaderFactory {
+  public static ImageReader getImageReader(ImageTypes types) {
+    if (ImageTypes.JPG == types) {
+      return new JpgReader();
+    } else if (ImageTypes.PNG == types) {
+      return new PngReader();
+    } else if (ImageTypes.BMP == types) {
+      return new BmpReader();
+    }
+    throw new IllegalArgumentException("Неизвестный тип картинки");
+  }
+}
---
+++ Java_Core/src/main/java/Level_6/lvl_6_task_31/lvl_6_task_31.java
@@ -0,0 +1,14 @@
+package Level_6.lvl_6_task_31;
+
+/*
+Factory method pattern
+*/
+
+import Java_Core.Level_6.lvl_6_task_31.common.ImageReader;
+import Java_Core.Level_6.lvl_6_task_31.common.ImageTypes;
+
+public class lvl_6_task_31 {
+  public static void main(String[] args) {
+    ImageReader reader = ImageReaderFactory.getImageReader(ImageTypes.JPG);
+  }
+}
