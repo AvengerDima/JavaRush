7c23ea9300398bc339da2b7b079e4cc74d2b769b
Avenger <Dima_c.2011@mail.ru>
2021-06-13 13:21:19 +0300

    Человек

---
+++ Java_Core/src/main/java/Level_9/lvl_9_task_21/Person.java
@@ -0,0 +1,21 @@
+package Level_9.lvl_9_task_21;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private Date birthDate;
+
+    public Person(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+}
