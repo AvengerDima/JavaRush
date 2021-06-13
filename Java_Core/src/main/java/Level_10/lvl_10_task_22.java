7469b14fb558ea5777da1b70127fb07c52500167
Avenger <Dima_c.2011@mail.ru>
2021-06-13 13:54:18 +0300

    Переопределение Сериализации в Потоке

---
+++ Java_Core/src/main/java/Level_10/lvl_10_task_22.java
@@ -0,0 +1,42 @@
+package Level_10;
+
+/*
+Переопределение сериализации в потоке
+*/
+
+import java.io.*;
+
+public class lvl_10_task_22 implements Serializable, AutoCloseable {
+    transient private FileOutputStream stream;
+    private final String fileName;
+
+    public lvl_10_task_22(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+        this.stream = new FileOutputStream(fileName);
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        this.stream = new FileOutputStream(fileName, true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
