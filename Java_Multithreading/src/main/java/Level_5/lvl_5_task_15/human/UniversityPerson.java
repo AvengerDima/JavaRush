16ba9b98fe89df38436f1054a988a95b157f43e2
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:20:28 +0300

    Работник Университета

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/human/UniversityPerson.java
@@ -0,0 +1,17 @@
+package Level_5.lvl_5_task_15.human;
+
+public class UniversityPerson extends Human {
+    private University university;
+
+    public UniversityPerson(String name, int age) {
+        super(name, age);
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+}
