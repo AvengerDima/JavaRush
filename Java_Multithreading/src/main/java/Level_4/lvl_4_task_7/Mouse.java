833ba25073482b53e57757f2f0b490add0bb9240
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:39:39 +0300

    Мышь

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_7/Mouse.java
@@ -0,0 +1,13 @@
+package Level_4.lvl_4_task_7;
+
+public class Mouse implements Pet, Sayable {
+    @Override
+    public Sayable toSayable(int i) {
+        return this;
+    }
+
+    @Override
+    public String say() {
+        return "Мышь пищит.";
+    }
+}
---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_8/Mouse.java
@@ -0,0 +1,17 @@
+package Level_4.lvl_4_task_8;
+
+/*
+обратите внимание, как именно Mouse отличается от Dog
+Этот класс - привычный для вас.
+*/
+public class Mouse implements Pet, Sayable {
+    @Override
+    public Sayable toSayable(int i) {
+        return this;
+    }
+
+    @Override
+    public String say() {
+        return "Мышь пищит.";
+    }
+}
