5583e7355e2246103e0441a14c6d53d9cff1ce9e
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:52:06 +0300

    Модель

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/model/Model.java
@@ -0,0 +1,10 @@
+package Level_4.lvl_4_task_13.model;
+
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+    void loadDeletedUsers();
+    void loadUserById(long userId);
+    void deleteUserById(long id);
+    void changeUserData(String name, long id, int level);
+}
