8f6f6b7becebb720c5065d7d3e92e83aa1bfb4bd
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:14:55 +0300

    Студент

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/human/Student.java
@@ -0,0 +1,54 @@
+package Level_5.lvl_5_task_15.human;
+
+import java.util.Date;
+
+public class Student extends UniversityPerson {
+    private double averageGrade;
+    private Date beginningOfSession;
+    private Date endOfSession;
+    private int course;
+
+    public Student(String name, int age, double averageGrade) {
+        super(name, age);
+        this.averageGrade = averageGrade;
+    }
+
+    public void live() {
+        learn();
+    }
+
+    public void learn() {
+    }
+
+    public String getPosition() {
+        return "Студент";
+    }
+
+    public void incAverageGrade(double delta) {
+        setAverageGrade(getAverageGrade() + delta);
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
+    }
+
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+}
