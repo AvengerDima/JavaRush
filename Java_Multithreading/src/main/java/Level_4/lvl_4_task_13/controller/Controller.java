d7583cacba6f0d95eb138364b6fe5739d6d6ac2e
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:53:52 +0300

    Контроллер

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/controller/Controller.java
@@ -0,0 +1,48 @@
+package Level_4.lvl_4_task_13.controller;
+
+import Level_4.lvl_4_task_13.model.Model;
+import Level_4.lvl_4_task_13.view.EditUserView;
+import Level_4.lvl_4_task_13.view.UsersView;
+
+public class Controller { //получение запроса от клиента
+    private Model model;
+    private UsersView usersView;
+    private EditUserView editUserView;
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void onShowAllUsers() {
+        model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+    public void onOpenUserEditForm(long userId) {
+        model.loadUserById(userId);
+        editUserView.refresh(model.getModelData());
+    }
+
+    public void onUserDelete(long id) {
+        model.deleteUserById(id);
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onUserChange(String name, long id, int level) {
+        model.changeUserData(name, id, level);
+        usersView.refresh(model.getModelData());
+    }
+}
