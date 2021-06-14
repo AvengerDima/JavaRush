cb978b02a6d731cf9eb498cceec76ae67370d873
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:15:33 +0300

    Седан

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/car/Sedan.java
@@ -0,0 +1,13 @@
+package Level_5.lvl_5_task_15.car;
+
+public class Sedan extends Car {
+    public Sedan(int numberOfPassengers) {
+        super(SEDAN, numberOfPassengers);
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        final int MAX_SEDAN_SPEED = 120;
+        return MAX_SEDAN_SPEED;
+    }
+}
