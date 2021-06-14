ed20969c4878c2dda56298ddb575cdd8cce22b12
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:17:27 +0300

    Адрес

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/user/Address.java
@@ -0,0 +1,31 @@
+package Level_5.lvl_5_task_15.user;
+
+public class Address {
+    private String country;
+    private String city;
+    private String house;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+}
