86b71d630f63dbacb053248899a1dc15a255a096
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:15:46 +0300

    Солдат

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/human/Soldier.java
@@ -0,0 +1,15 @@
+package Level_5.lvl_5_task_15.human;
+
+public class Soldier extends Human {
+    public Soldier(String name, int age) {
+        super(name, age);
+    }
+
+    public void live() {
+        fight();
+    }
+
+    public void fight() {
+
+    }
+}
