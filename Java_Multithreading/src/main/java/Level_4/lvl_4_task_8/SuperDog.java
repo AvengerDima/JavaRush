eec13d45b533c617bd1b3b46538d46969d510514
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:41:38 +0300

    Собака

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_8/SuperDog.java
@@ -0,0 +1,12 @@
+package Level_4.lvl_4_task_8;
+
+import java.text.SimpleDateFormat;
+
+public abstract class SuperDog {
+    protected String getSuperQuotes() {
+        //some logic here
+        return " *** ";
+    }
+
+    protected SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy EEE");
+}
