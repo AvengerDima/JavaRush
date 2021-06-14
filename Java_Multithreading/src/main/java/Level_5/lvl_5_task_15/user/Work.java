0928437995c565043ce7ed6b967ebcf14c24919d
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:16:19 +0300

    Работа

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/user/Work.java
@@ -0,0 +1,13 @@
+package Level_5.lvl_5_task_15.user;
+
+public class Work {
+    private String boss;
+
+    public String getBoss() {
+        return boss;
+    }
+
+    public void setBoss(String boss) {
+        this.boss = boss;
+    }
+}
