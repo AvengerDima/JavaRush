9a1c5d1048e4d0f4580805740afcfa042fc77ccb
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:13:01 +0300

    Подставка

---
+++ Java_Multithreading/src/main/java/Level_3/lvl_3_task_12/Stand.java
@@ -0,0 +1,64 @@
+package Level_3.lvl_3_task_12;
+
+/**
+ * Подставка, с помощью которой отражаем мячик.
+ */
+public class Stand extends BaseObject {
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    //скорость
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+    }
+
+    /**
+     * Метод передвигает подставку в соответствии с текущим значением direction.
+     */
+    void move() {
+        double dx = speed * direction;
+        x = x + dx;
+
+        checkBorders(radius, lvl_3_task_12.game.getWidth() - radius + 1, 1, lvl_3_task_12.game.getHeight() + 1);
+    }
+
+    /**
+     * direction устанавливается равным -1
+     */
+    void moveLeft() {
+        direction = -1;
+    }
+
+    /**
+     * direction устанавливается равным +1
+     */
+    void moveRight() {
+        direction = 1;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    /**
+     * Отрисовываем себя на холсте
+     */
+    @Override
+    void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
+    }
+}
