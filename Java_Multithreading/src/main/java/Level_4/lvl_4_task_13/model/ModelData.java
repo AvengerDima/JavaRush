bfa16193c3ce2c6cee94a6f947ae546e90685cfa
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:52:19 +0300

    Данные Модели

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/model/ModelData.java
@@ -0,0 +1,35 @@
+package Level_4.lvl_4_task_13.model;
+
+import Level_4.lvl_4_task_13.bean.User;
+
+import java.util.List;
+
+public class ModelData {
+    private List<User> users; //список пользователей для отображения
+    private User activeUser;
+    private boolean displayDeletedUserList;
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
+    }
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
