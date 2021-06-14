e328132b87f7141549c68f455bd83ac0401fd01a
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:14:09 +0300

    Кабриолет

---
+++ Java_Multithreading/src/main/java/Level_5/lvl_5_task_15/car/Cabriolet.java
@@ -0,0 +1,13 @@
+package Level_5.lvl_5_task_15.car;
+
+public class Cabriolet extends Car {
+    public Cabriolet(int numberOfPassengers) {
+        super(CABRIOLET, numberOfPassengers);
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        final int MAX_CABRIOLET_SPEED = 90;
+        return MAX_CABRIOLET_SPEED;
+    }
+}
