4bea331cf12d417a4845faaa7ac30e20bd8baace
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:00:41 +0300

    Задача на final, …

---
+++ Java_Multithreading/src/main/java/Level_3/lvl_3_task_2.java
@@ -0,0 +1,21 @@
+package Level_3;
+
+/*
+Запрети переопределение
+*/
+
+public class lvl_3_task_2 {
+    public static class Listener {
+        public final void onMouseDown(int x, int y) {
+            // Do something when the mouse down event occurs
+        }
+
+        public void onMouseUp(int x, int y) {
+            // Do something when the mouse up event occurs
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java_Multithreading/src/main/java/Level_3/lvl_3_task_3.java
@@ -0,0 +1,21 @@
+package Level_3;
+
+/*
+Запрети создание экземпляров класса
+*/
+
+public class lvl_3_task_3 {
+    public static abstract class Listener {
+        public void onMouseDown(int x, int y) {
+            // Do something when the mouse down event occurs
+        }
+
+        public void onMouseUp(int x, int y) {
+            // Do something when the mouse up event occurs
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
