b8be2d66cb5c330cd8d787b6c006798ca6494a5f
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:51:13 +0300

    Пользовательский Сервис

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/model/service/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package Level_4.lvl_4_task_13.model.service;
+
+import Level_4.lvl_4_task_13.Util;
+import Level_4.lvl_4_task_13.bean.User;
+import Level_4.lvl_4_task_13.dao.UserDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private UserDao userDao = new UserDao();
+
+    @Override
+    public User deleteUser(long id) {
+        User user = userDao.getUserById(id);
+        Util.markDeleted(user);
+        userDao.createOrUpdate(user);
+
+        return user;
+    }
+
+    @Override
+    public User createOrUpdateUser(String name, long id, int level) {
+        User user = new User(name, id, level);
+        return userDao.createOrUpdate(user);
+    }
+
+    @Override
+    public List<User> getUsersByName(String name) {
+        return userDao.getUsersByName(name);
+    }
+
+    @Override
+    public List<User> getAllDeletedUsers() {
+        List<User> result = new ArrayList<>();
+        for (User user : userDao.getAllUsers()) {
+            if (Util.isUserDeleted(user)) {
+                result.add(user);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<User> getUsersBetweenLevels(int fromLevel, int toLevel) {
+        //it's better to get all users from DAO by one DB request, but we will use what we have
+        List<User> result = new ArrayList<>();
+        for (int i = fromLevel; i <= toLevel; i++) {
+            result.addAll(userDao.getUsersByLevel(i));
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<User> filterOnlyActiveUsers(List<User> allUsers) {
+        //will not change allUsers list, create new one instead of that
+        List<User> result = new ArrayList<>();
+        for (User user : allUsers) {
+            if (User.NULL_USER != user && !Util.isUserDeleted(user)) {
+                result.add(user);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public User getUsersById(long userId) {
+        return userDao.getUsersById(userId);
+    }
+}
