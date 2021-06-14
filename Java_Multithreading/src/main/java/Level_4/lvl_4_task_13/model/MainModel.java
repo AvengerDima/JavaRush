806d6c49726e795f073e2270a335af45e26d9c87
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:52:32 +0300

    Главная Модель

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/model/MainModel.java
@@ -0,0 +1,53 @@
+package Level_4.lvl_4_task_13.model;
+
+import Level_4.lvl_4_task_13.bean.User;
+import Level_4.lvl_4_task_13.model.service.UserService;
+import Level_4.lvl_4_task_13.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(getAllUsers());
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userService.deleteUser(id);
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getAllUsers());
+    }
+
+    private List<User> getAllUsers() {
+        return userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        loadUsers();
+    }
+}
