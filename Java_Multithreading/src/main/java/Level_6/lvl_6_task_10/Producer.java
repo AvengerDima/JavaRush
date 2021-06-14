bdfb7d83eb23b82747bec66ff4b10104d8a56cda
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:34:28 +0300

    Производитель

---
+++ Java_Multithreading/src/main/java/Level_6/lvl_6_task_10/Producer.java
@@ -0,0 +1,23 @@
+package Level_6.lvl_6_task_10;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable {
+    private BlockingQueue queue;
+
+    public Producer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            int i = 0;
+            while (true) {
+                queue.put(String.valueOf(i++));
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
