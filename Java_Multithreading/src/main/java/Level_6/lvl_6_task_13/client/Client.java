c4ae04b43d2146da4d0b8f27d389edbcee3c5c81
Avenger <Dima_c.2011@mail.ru>
2021-06-14 16:35:03 +0300

    Клиент

---
+++ Java_Multithreading/src/main/java/Level_6/lvl_6_task_13/client/Client.java
@@ -0,0 +1,151 @@
+package Level_6.lvl_6_task_13.client;
+
+import Level_6.lvl_6_task_13.Connection;
+import Level_6.lvl_6_task_13.ConsoleHelper;
+import Level_6.lvl_6_task_13.Message;
+import Level_6.lvl_6_task_13.MessageType;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected;
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Введите ваше имя:");
+        return ConsoleHelper.readString();
+    }
+
+    public class SocketThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                connection = new Connection(new Socket(getServerAddress(), getServerPort()));
+
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+
+                if (message.getType() == MessageType.NAME_REQUEST) { // Сервер запросил имя пользователя
+                    String name = getUserName(); // Запрашиваем ввод имени с консоли
+                    connection.send(new Message(MessageType.USER_NAME, name)); // Отправляем имя на сервер
+                } else if (message.getType() == MessageType.NAME_ACCEPTED) { // Cервер принял имя
+                    // Сообщаем главному потоку, что он может продолжить работу
+                    notifyConnectionStatusChanged(true);
+                    return;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            // Цикл обработки сообщений сервера
+            while (true) {
+                Message message = connection.receive();
+
+                if (message.getType() == MessageType.TEXT) { // Сервер прислал сообщение с текстом
+                    processIncomingMessage(message.getData());
+                } else if (message.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (message.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message); //вывод текста сообщения в консоль
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            // Выводим информацию о добавлении участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' присоединился к чату.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            // Выводим информацию о выходе участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' покинул чат.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+            clientConnected = false;
+        }
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true); //помечаем поток как daemon
+        socketThread.start();
+
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            return;
+        }
+
+        if (clientConnected) {
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        } else {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        }
+
+        // Пока не будет введена команда exit, считываем сообщения с консоли и отправляем их на сервер
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+            if (text.equalsIgnoreCase("exit"))
+                break;
+
+            if (shouldSendTextFromConsole())
+                sendTextMessage(text);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
