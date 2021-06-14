2b8ff8ba763b118ba15038561640c573238b0cb9
Avenger <Dima_c.2011@mail.ru>
2021-06-14 15:52:51 +0300

    Поддельная Модель

---
+++ Java_Multithreading/src/main/java/Level_4/lvl_4_task_13/model/FakeModel.java
@@ -0,0 +1,43 @@
+package Level_4.lvl_4_task_13.model;
+
+import Level_4.lvl_4_task_13.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("A", 1, 1));
+        users.add(new User("B", 2, 1));
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        throw new UnsupportedOperationException();
+    }
+}
