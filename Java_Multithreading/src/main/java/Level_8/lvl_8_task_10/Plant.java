a9d7b45d2facd3835e8de6dd0c0ba7f0920afc45
Avenger <Dima_c.2011@mail.ru>
2021-06-15 19:39:57 +0300

    Растение

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_10/Plant.java
@@ -0,0 +1,12 @@
+package Level_8.lvl_8_task_10;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Plant extends Character {
+    private final static AtomicInteger idSequence = new AtomicInteger();
+    private final int id = idSequence.incrementAndGet();
+
+    protected int getId() {
+        return id;
+    }
+}
