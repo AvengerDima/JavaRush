5a579c6b595755e419992ff2a22de33427f7ccff
Avenger <Dima_c.2011@mail.ru>
2021-06-15 19:39:34 +0300

    Персонаж

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_10/Character.java
@@ -0,0 +1,25 @@
+package Level_8.lvl_8_task_10;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class Character implements Runnable {
+    protected abstract int getId();
+
+    public void run() {
+        System.out.println(toString() + " вступил в игру");
+        doSomething();
+        System.out.println(toString() + " умер");
+    }
+
+    private void doSomething() {
+        try {
+            Thread.currentThread().sleep(ThreadLocalRandom.current().nextInt(10, 100));
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " #" + getId();
+    }
+}
