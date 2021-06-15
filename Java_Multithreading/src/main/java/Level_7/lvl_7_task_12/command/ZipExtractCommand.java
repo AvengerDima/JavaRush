1c74ebea1af02e38d0889ffe6a7e957bad72c803
Avenger <Dima_c.2011@mail.ru>
2021-06-15 18:31:33 +0300

    Команда Распаковки Zip

---
+++ Java_Multithreading/src/main/java/Level_7/lvl_7_task_12/command/ZipExtractCommand.java
@@ -0,0 +1,28 @@
+package Level_7.lvl_7_task_12.command;
+
+import Level_7.lvl_7_task_12.ConsoleHelper;
+import Level_7.lvl_7_task_12.ZipFileManager;
+import Level_7.lvl_7_task_12.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
+
+            ConsoleHelper.writeMessage("Архив был распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
+        }
+    }
+}
