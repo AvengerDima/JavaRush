a5509150efabfad8401eb50fa2345ae406799c57
Avenger <Dima_c.2011@mail.ru>
2021-06-15 18:48:20 +0300

    Задача на Приоритет Нитей

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_5/lvl_8_task_5.java
@@ -0,0 +1,24 @@
+package Level_8.lvl_8_task_5;
+
+/*
+Приоритеты в Threads
+*/
+
+public class lvl_8_task_5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.print(new MyThread().getPriority() + " ");
+        }
+        //output
+        //1 2 3 4 5 6 7 8 9 10 1 2
+
+        System.out.println();
+        ThreadGroup group = new ThreadGroup("someName");
+        group.setMaxPriority(7);
+        for (int i = 0; i < 12; i++) {
+            System.out.print(new MyThread(group, "name" + i).getPriority() + " ");
+        }
+        //output
+        //3 4 5 6 7 7 7 7 1 2 3 4
+    }
+}
