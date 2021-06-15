4da121dafb41491b26edb79d3b09df15c34f044f
Avenger <Dima_c.2011@mail.ru>
2021-06-15 18:31:00 +0300

    Команда Просмотра Zip

---
+++ Java_Multithreading/src/main/java/Level_7/lvl_7_task_12/command/ZipContentCommand.java
@@ -0,0 +1,25 @@
+package Level_7.lvl_7_task_12.command;
+
+import Level_7.lvl_7_task_12.ConsoleHelper;
+import Level_7.lvl_7_task_12.FileProperties;
+import Level_7.lvl_7_task_12.ZipFileManager;
+
+import java.util.List;
+
+public class ZipContentCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+    }
+}
