d99712459d49bee5f2cf8425d7601e5ad2ae4b9e
Avenger <Dima_c.2011@mail.ru>
2021-06-15 18:27:29 +0300

    Исполнитель Команд

---
+++ Java_Multithreading/src/main/java/Level_7/lvl_7_task_12/CommandExecutor.java
@@ -0,0 +1,26 @@
+package Level_7.lvl_7_task_12;
+
+import Level_7.lvl_7_task_12.command.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
+    static {
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
+
+    private CommandExecutor() {
+    }
+
+    public static void execute(Operation operation) throws Exception {
+        allKnownCommandsMap.get(operation).execute();
+    }
+}
