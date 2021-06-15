eb0cad8781810ee8a555fa76409cd7a370dd4557
Avenger <Dima_c.2011@mail.ru>
2021-06-15 17:38:55 +0300

    Почта

---
+++ Java_Multithreading/src/main/java/Level_7/lvl_7_task_10/Mail.java
@@ -0,0 +1,13 @@
+Почтpackage Level_7.lvl_7_task_10;
+
+public class Mail {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
