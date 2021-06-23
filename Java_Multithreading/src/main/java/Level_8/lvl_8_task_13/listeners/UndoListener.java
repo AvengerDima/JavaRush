118cc3d1d7a2c18bfe9830e92be4c476a69fc01e
Avenger <Dima_c.2011@mail.ru>
2021-06-23 16:25:01 +0300

    Слушатель Отмены

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_13/listeners/UndoListener.java
@@ -0,0 +1,18 @@
+package Level_8.lvl_8_task_13.listeners;
+
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoManager;
+
+public class UndoListener implements UndoableEditListener {
+    private UndoManager undoManager;
+
+    public UndoListener(UndoManager undoManager) {
+        this.undoManager = undoManager;
+    }
+
+    @Override
+    public void undoableEditHappened(UndoableEditEvent e) {
+        undoManager.addEdit(e.getEdit());
+    }
+}
