b63e4da8be40403fbfea564d72d044dcc2bed035
Avenger <Dima_c.2011@mail.ru>
2021-06-23 16:25:30 +0300

    Слушатель Меню Отмены

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_13/listeners/UndoMenuListener.java
@@ -0,0 +1,35 @@
+package Level_8.lvl_8_task_13.listeners;
+
+import Level_8.lvl_8_task_13.View;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class UndoMenuListener implements MenuListener {
+    private View view;
+    private JMenuItem undoMenuItem;
+    private JMenuItem redoMenuItem;
+
+    public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+        this.view = view;
+        this.undoMenuItem = undoMenuItem;
+        this.redoMenuItem = redoMenuItem;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent menuEvent) {
+        undoMenuItem.setEnabled(view.canUndo());
+        redoMenuItem.setEnabled(view.canRedo());
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
