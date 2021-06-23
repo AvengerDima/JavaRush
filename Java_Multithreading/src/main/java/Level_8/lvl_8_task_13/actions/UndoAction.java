85476f7c78b506f7193bc7864c143f21c229cf42
Avenger <Dima_c.2011@mail.ru>
2021-06-23 16:21:24 +0300

    Отменить Действие

---
+++ Java_Multithreading/src/main/java/Level_8/lvl_8_task_13/actions/UndoAction.java
@@ -0,0 +1,19 @@
+package Level_8.lvl_8_task_13.actions;
+
+import Level_8.lvl_8_task_13.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class UndoAction extends AbstractAction {
+    private View view;
+
+    public UndoAction(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        view.undo();
+    }
+}
