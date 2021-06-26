53ea17518c168d07a65c8456793d1f853d0e1ccf
Avenger <Dima_c.2011@mail.ru>
2021-06-26 19:04:27 +0300

    Уровень

---
+++ Java_Multithreading/src/main/java/Level_9/lvl_9_task_12/Level.java
@@ -0,0 +1,8 @@
+package Level_9.lvl_9_task_12;
+
+public class Level {
+    public static final int INFO = 100;
+    public static final int WARN = 200;
+    public static final int ERROR = 300;
+    public static final int FATAL = 400;
+}
