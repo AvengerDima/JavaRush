60efceedf8f0006dc04f641e0e7628e905d3b3c8
Avenger <Dima_c.2011@mail.ru>
2021-06-26 19:03:54 +0300

    Биатлон

---
+++ Java_Multithreading/src/main/java/Level_9/lvl_9_task_11/lvl_9_task_11.java
@@ -0,0 +1,23 @@
+package Level_9.lvl_9_task_11;
+
+/*
+Это архаичное слово "Биатлон"
+*/
+
+public class lvl_9_task_11 {
+    public static void main(String[] args) {
+        Game game = new Game();
+        printDelimiter();
+        game.startCompetition();
+        System.out.println("\n\nВСЕ РЕЗУЛЬТАТЫ:");
+        game.printAllResults();
+        printDelimiter();
+        System.out.println("\n\nСПОРТСМЕНЫ С ТАКИМ ЖЕ ИТОГОВЫМ ВРЕМЕНЕМ, КАК У КОНТРОЛЬНОГО СПОРТСМЕНА:");
+        game.printSportsmanWithEqualTime();
+        printDelimiter();
+    }
+
+    private static void printDelimiter() {
+        System.out.println("=================================================================================================");
+    }
+}
