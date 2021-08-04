c04ca0a423d5d281892e8268bdf8e16bde603943
Avenger <Dima_c.2011@mail.ru>
2021-08-04 15:41:00 +0300

    Блюдо

---
+++ Java_Multithreading/src/main/java/Level_9/lvl_9_task_13/kitchen/Dish.java
@@ -0,0 +1,32 @@
+package Level_9.lvl_9_task_13.kitchen;
+
+public enum Dish { //Блюдо
+    FISH(25),
+    STEAK(30),
+    SOUP(15),
+    JUICE(5),
+    WATER(3);
+
+    private int duration;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    Dish(int duration) {
+        this.duration = duration;
+    }
+
+    public static String allDishesToString() {
+        String result = "";
+
+        for (Dish dish : Dish.values()) {
+            if ("".equals(result)) {
+                result += dish.name();
+            } else {
+                result += ", " + dish.name();
+            }
+        }
+        return result;
+    }
+}
