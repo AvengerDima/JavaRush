c514ca9dc942bef0da0d862f539071373b9f739b
Dmitry Semkov <dimac2009@gmail.com>
2021-11-07 14:35:57 +0200

    Работа с Файлами и Архивами

---
+++ Java_Collections/src/main/java/Level_1/lvl_1_task_11.java
@@ -0,0 +1,22 @@
+package Level_1;
+
+/*
+Исследуем Path
+*/
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class lvl_1_task_11 {
+    public static void main(String[] args) throws IOException {
+        Path path1 = Paths.get("D:/test/data/firstDir");
+        Path path2 = Paths.get("D:/test/data/secondDir/third");
+        Path resultPath = getDiffBetweenTwoPaths(path1, path2);
+        System.out.println(resultPath);   //expected output '../secondDir/third' or '..\secondDir\third'
+    }
+
+    public static Path getDiffBetweenTwoPaths(Path path1, Path path2) {
+        return path1.relativize(path2);
+    }
+}
