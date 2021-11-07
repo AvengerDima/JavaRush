d25303f1a403da5a0088195d68130a14c398bef2
Dmitry Semkov <dimac2009@gmail.com>
2021-11-07 14:39:07 +0200

    Запись в Существующий Файл

---
+++ Java_Collections/src/main/java/Level_2/lvl_2_task_1.java
@@ -0,0 +1,22 @@
+package Level_2;
+
+/*
+Запись в существующий файл
+*/
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class lvl_2_task_1 {
+    public static void main(String... args) {
+        try (RandomAccessFile raf = new RandomAccessFile(args[0], "rw")) {
+            long number = Long.parseLong(args[1]);
+            long length = raf.length();
+            number = number > length ? length : number;
+            raf.seek(number);
+            raf.write(args[2].getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
