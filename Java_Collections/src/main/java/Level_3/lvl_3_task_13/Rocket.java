35cec1618ff7f30bc0378c35eb9d87e244c350ca
Dmitry Semkov <dimac2009@gmail.com>
2021-11-19 19:33:09 +0200

    Ракета

---
+++ Java_Collections/src/main/java/Level_3/lvl_3_task_13/Rocket.java
@@ -0,0 +1,27 @@
+package Level_3.lvl_3_task_13;
+
+/**
+ * Класс для объектов-ракета
+ */
+public class Rocket extends BaseObject {
+
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move() {
+        y--;
+    }
+}
