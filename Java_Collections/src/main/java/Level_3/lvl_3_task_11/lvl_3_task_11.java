c6a92f78e529b965cbef4da41c2a9368cee84010
Dmitry Semkov <dimac2009@gmail.com>
2021-11-19 19:32:21 +0200

    Создание Класса по Строке xml

---
+++ Java_Collections/src/main/java/Level_3/lvl_3_task_11/lvl_3_task_11.java
@@ -0,0 +1,40 @@
+package Level_3.lvl_3_task_11;
+
+/*
+Создание класса по строке xml
+*/
+
+import java.io.StringReader;
+
+public class lvl_3_task_11 {
+    public static void main(String[] args) throws JAXBException {
+        String xmlData =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                        "<shop>\n" +
+                        "    <goods>\n" +
+                        "        <names>S1</names>\n" +
+                        "        <names>S2</names>\n" +
+                        "    </goods>\n" +
+                        "    <count>12</count>\n" +
+                        "    <profit>123.4</profit>\n" +
+                        "    <secretData>String1</secretData>\n" +
+                        "    <secretData>String2</secretData>\n" +
+                        "    <secretData>String3</secretData>\n" +
+                        "    <secretData>String4</secretData>\n" +
+                        "    <secretData>String5</secretData>\n" +
+                        "</shop>";
+
+        StringReader reader = new StringReader(xmlData);
+
+        JAXBContext context = JAXBContext.newInstance(getClassName());
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        Object o = unmarshaller.unmarshal(reader);
+
+        System.out.println(o.toString());
+    }
+
+    public static Class getClassName() {
+        return Shop.class;  //your class name
+    }
+}
