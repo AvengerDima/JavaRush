e05a1a03cefed5650fffeeb30a930090dedaa2d5
Dmitry Semkov <dimac2009@gmail.com>
2021-11-19 19:27:29 +0200

    Гоночный Велосипед

---
+++ Java_Collections/src/main/java/Level_3/lvl_3_task_5/RacingBike.java
@@ -0,0 +1,33 @@
+package Level_3.lvl_3_task_5;
+
+public class RacingBike extends Motorbike {
+    private String owner;
+    private int age;
+
+    public RacingBike(String name, String owner, int age) {
+        super(name);
+        this.owner = owner;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "RacingBike{" +
+                "name='" + name + '\'' +
+                ", owner='" + owner + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
