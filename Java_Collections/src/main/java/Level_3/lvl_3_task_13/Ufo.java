d777f4116bf641a68f8f2e7e82394846c75162d8
Dmitry Semkov <dimac2009@gmail.com>
2021-11-19 19:32:54 +0200

    НЛО

---
+++ Java_Collections/src/main/java/Level_3/lvl_3_task_13/Ufo.java
@@ -0,0 +1,41 @@
+package Level_3.lvl_3_task_13;
+
+public class Ufo extends BaseObject {
+
+    //картинка корабля для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, lvl_3_task_13.game.getWidth() - radius + 1, radius - 1, lvl_3_task_13.game.getHeight() / 2 - 1);
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0)
+            fire();
+    }
+
+    public void fire() {
+        lvl_3_task_13.game.getBombs().add(new Bomb(x, y + 3));
+    }
+}
